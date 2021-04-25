3d9f53dcde34106821b46dfc6d3eb9936367d5dc
Jeffrey Follestad <jeffrey.follestad@gmail.com>
2021-04-25 16:41:48 -0700

    generate hour rows in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,55 @@
-var button = $('.saveBtn')
 
-//Add current date at the top of the planner 
+const WORK_START_HOUR = 9; // 9am
+const HOURS_IN_WORK_DAY = 8; // 9am+8 = 5pm.
+
+function displayCurrentTime() {
+  let currentDateTime = moment().format('dddd, MMMM Do');
+  $("#current-time-header").text(currentDateTime);
+}
 
 $(document).ready(function () {
-  var date = moment().format('dddd MMMM Do');
-
-  $('#currentDay').text(date);
+  displayCurrentTime();
 
   //Create code that adds in business hours (9am - 5pm) into file for adding in hourly events to the left of the 
   //event text box
-
+  let rowContainer = $("#row-container");
+  for (var index = 0; index <= HOURS_IN_WORK_DAY; index++) {
+    let currentHour = index + WORK_START_HOUR;
+    let row = createHourRow(currentHour, index);
+    rowContainer.append(row);
+  }
 
   //Set styling so that business hours that have passed are greyed out;  if / else for time component 
   //Set styling so that the current time/hour is a red color;
   //Set styling so that hours that have not yet occured are in green 
 
-
-  function currentTime() {
-    var current = moment().hours()
-    var time = $('.calendar-event');
-
-    //    console.log(current);
-
-    time.each(function () {
-      var hour = parseInt($(this).attr('id'))
-
-    //  console.log(hour);
-
-      if (hour === current) {
-        $(this).children('.col-sm-10').attr('class', 'present col-sm-10 description')
-    //    console.log(('present' + hour + current))
-      } else if (current > hour) {
-        $(this).children('.col-sm-10').attr('class', 'past col-sm-10 description')
-    //    console.log(('past' + hour + current))
-      } else {
-        $(this).children('.col-sm-10').attr('class', 'future col-sm-10 description')
-    //    console.log(('future' + hour + current))
-      }
-    })
-  }
-  currentTime()
-
   //Set up local storage to store persistent changes in events by timeblock
-
-  button.on('click', function (event) {
-    event.preventDefault();
-    var text = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
-    var parent = $(this).parent().attr('id');
-    localStorage.setItem(parent, JSON.stringify(text));
-
-  })
-
-  $('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
-  $('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''));
-  $('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
-  $('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
-  $('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
-  $('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
-  $('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
-  $('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
-  $('#17 textarea').val(localStorage.getItem('17')).replace(/['"]+/g, '');
-//  console.log(parent);
-
-  //Use styling to transform pointer into a disk icon at the time of click event (saving of event)
-
-  button.on('click', function (event) {
-    $('selector').css('cursor', 'i')
-  })
-  
-
-
+  // button.on('click', function (event) {
+  //   event.preventDefault();
+  //   var text = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
+  //   var parent = $(this).parent().attr('id');
+  //   localStorage.setItem(parent, JSON.stringify(text));
+  // })
 });
+
+function createHourRow(hour, index) {
+  let hourLabel = document.createElement("div");
+  hourLabel.className = "col-sm-1 hour text-right";
+  hourLabel.innerText = hour; // TODO: Want to add am/pm suffix.
+
+  let inputField = document.createElement("textarea");
+  inputField.className = "col-sm-10 description";
+
+  let saveButton = document.createElement("button");
+  saveButton.className = "col-sm-1 saveBtn btn";
+  saveButton.innerHTML = '<i class="far fa-save fa-lg"></i>';
+  saveButton.dataset.rowIndex = index;
+  // saveButton.addEventListener("click", selectedAnswer);
+
+  let rowElement = document.createElement("div");
+  rowElement.className = "row calendar-event";
+  rowElement.appendChild(hourLabel);
+  rowElement.appendChild(inputField);
+  rowElement.appendChild(saveButton);
+  return rowElement;
+}
