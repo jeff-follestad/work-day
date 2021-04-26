1eb21e13bb62e5ff41cb3e0473978a8f1da8cf7f
Jeffrey Follestad <jeffrey.follestad@gmail.com>
2021-04-25 17:18:53 -0700

    remove unneeded comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,20 +19,7 @@ function displayHourRows() {
 $(document).ready(function () {
   displayCurrentTime();
   displayHourRows();
-
-  //Set up local storage to store persistent changes in events by timeblock
-  // button.on('click', function (event) {
-  //   event.preventDefault();
-  //   var text = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
-  //   var parent = $(this).parent().attr('id');
-  //   localStorage.setItem(parent, JSON.stringify(text));
-  // })
 });
-
-
-// Set styling so that business hours that have passed are greyed out;  if / else for time component 
-// Set styling so that the current time/hour is a red color;
-// Set styling so that hours that have not yet occured are in green 
 
 function createHourRow(hour) {
   let hourLabel = document.createElement("div");
